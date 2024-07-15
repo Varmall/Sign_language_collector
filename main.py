4ea8f75d61188c6cee3933ecff17fe75ad424ab4
Varmall <p.antonowiczz@gmail.com>
2024-07-15 16:36:24 +0200

    Clause for path in config

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     config_path = Path('config.yml')
     with open(config_path, 'r') as f:
         cfg = yaml.safe_load(f)
-        print(cfg)
+    if not cfg['data_path'] or not Path(cfg['data_path']).is_dir() or str(Path(cfg['data_path'])) == ".":
+        data_path = Path.cwd() / "data"
+        data_path.mkdir(exist_ok=True)
+        cfg['data_path'] = str(data_path)
+    with open(config_path, 'w') as f:
+        yaml.safe_dump(cfg, f, sort_keys=False)
     app = QApplication(sys.argv)
     main_window = UiMainWindow(cfg)
     main_window.show()
