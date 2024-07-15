0837a933ea5e73ae629d17da91f8b31a1da542d6
Varmall <p.antonowiczz@gmail.com>
2024-07-15 13:29:19 +0200

    Modified path label display, added cboxes for number of frames per vid and number of vids per word

--- GUI/control_panel.py
+++ GUI/control_panel.py
@@ -12,10 +12,30 @@ class ControlPanel(QtWidgets.QFrame):
         self.current_path_label = QtWidgets.QLabel()
         self.set_path(Path(config['data_path']))
         self.current_path_label.setWordWrap(True)
+        self.current_path_label.setStyleSheet("QLabel:hover{background-color:rgb(210, 210, 210);}")
         self.path_button = QtWidgets.QPushButton('Change save path')
         self.select_words_label = QtWidgets.QLabel('Select Word:')
         self.select_words_cbox = QtWidgets.QComboBox()
         self.select_words_cbox.addItems(config['words'])
+        self.select_words_cbox.currentTextChanged.connect(lambda: parent.reset_example())
+
+        self.num_vid_layout = QtWidgets.QHBoxLayout()
+        self.num_vid_label = QtWidgets.QLabel("Number of Videos:")
+        self.num_vid_cbox = QtWidgets.QComboBox()
+        self.num_vid_cbox.addItems([str(x + 1) for x in range(50)])
+        self.num_vid_cbox.setCurrentText(str(config.get("videos_per_word", 10)))
+        self.num_vid_layout.addWidget(self.num_vid_label)
+        self.num_vid_layout.addStretch(1)
+        self.num_vid_layout.addWidget(self.num_vid_cbox)
+
+        self.num_frames_layout = QtWidgets.QHBoxLayout()
+        self.num_frames_label = QtWidgets.QLabel("Frames per video:")
+        self.num_frames_cbox = QtWidgets.QComboBox()
+        self.num_frames_cbox.addItems([str(x + 1) for x in range(19, 400, 5)])
+        self.num_frames_cbox.setCurrentText(str(config.get("frames_per_vid", 40)))
+        self.num_frames_layout.addWidget(self.num_frames_label)
+        self.num_frames_layout.addStretch(1)
+        self.num_frames_layout.addWidget(self.num_frames_cbox)
 
         # Layout stuff
         self.v_layout = QtWidgets.QVBoxLayout()
@@ -26,11 +46,13 @@ class ControlPanel(QtWidgets.QFrame):
         self.v_layout.addWidget(self.path_button)
         self.v_layout.addWidget(self.select_words_label)
         self.v_layout.addWidget(self.select_words_cbox)
+        self.v_layout.addLayout(self.num_vid_layout)
+        self.v_layout.addLayout(self.num_frames_layout)
         self.v_layout.addWidget(self.session_record_button)
         self.v_layout.addStretch(1)
         self.setLayout(self.v_layout)
 
     def set_path(self, path: Path):
-        self.current_path_label.setText(f"Save Path: {path}")
+        self.current_path_label.setText(f"Save Path: {path.relative_to(Path.cwd())}")
         self.current_path_label.setToolTip(f"{path.absolute()}")
 
