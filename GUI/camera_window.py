e1f24ba96cb0bfc2734480b51d555e63a7db2478
Varmall <p.antonowiczz@gmail.com>
2024-07-15 13:30:24 +0200

    Implemented example video display, implemented cboxes from control panel

--- GUI/camera_window.py
+++ GUI/camera_window.py
@@ -5,7 +5,8 @@ import yaml
 from PyQt6 import QtCore, QtGui, QtWidgets
 import cv2
 from PyQt6.QtCore import pyqtSignal, pyqtSlot
-from PyQt6.QtGui import QImage
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtMultimedia import QMediaPlayer
 
 from GUI.control_panel import ControlPanel
 from utils.data_handle import get_path
@@ -15,9 +16,11 @@ class UiMainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, config: dict):
         super().__init__()
+        self._video_frame = (0, 0)
         self._is_in_session = False
         self._is_recording = False
         self._frames_buffer = []
+        self.loaded_example_videos = {}
         self.delay = 3
         self.current_video = 0
         self.config = config
@@ -61,9 +64,19 @@ class UiMainWindow(QtWidgets.QMainWindow):
         self.resize(video_size[0] + self.control_panel.width() + 30, video_size[1])
         self.video_capture_thread.start()
 
+        self.video_example_display = QtWidgets.QLabel(parent=self)
+        self.video_example_display.setStyleSheet("background-color: rgb(200, 200, 0);")
+        self.video_example_display.setFixedSize(480, 270)
+        self.video_example_display.move(self.width()-self.video_example_display.width() - 12, 12)
+
         self.show()
-        print(self.frame.width(), self.frame.height())
         self.counter_label.move(self.frame.x() + int(video_size[0]/2), self.frame.y())
+        self.load_example_videos(Path("examples/yes"), "yes")
+
+        self.example_timer = QtCore.QTimer()
+        self.example_timer.setInterval(int(1000 / 30))
+        self.example_timer.timeout.connect(self.handle_example_timer)
+        self.example_timer.start()
 
     def toggle_recording_session(self):
         self.is_in_session = not self.is_in_session
@@ -87,6 +100,42 @@ class UiMainWindow(QtWidgets.QMainWindow):
             self._timer_counter = self.delay
             self._is_recording = True
 
+    def handle_example_timer(self):
+        current_word = self.control_panel.select_words_cbox.currentText()
+        if current_word not in self.loaded_example_videos:
+            self.load_example_videos(Path(f"examples/{current_word}"), current_word)
+        if not self.loaded_example_videos[current_word]:
+            self.video_example_display.setText("Video not found")
+            return
+        current_video = self.loaded_example_videos[current_word][self._video_frame[0]]
+        frame = current_video[self._video_frame[1]]
+        self.video_example_display.setPixmap(QPixmap(frame))
+        self._video_frame = (self._video_frame[0], self._video_frame[1]+1)
+        if self._video_frame[1] == len(current_video):
+            self._video_frame = (self._video_frame[0]+1, 0)
+        if self._video_frame[0] == len(self.loaded_example_videos[current_word]):
+            self._video_frame = (0, 0)
+
+    def reset_example(self):
+        self._video_frame = (0, 0)
+
+    def load_example_videos(self, dir_path: Path, word: str):
+        if word in self.loaded_example_videos:
+            return
+        video_paths = dir_path.glob('*.mp4')
+        self.loaded_example_videos[word] = {}
+        for i, video_path in enumerate(video_paths):
+            self.loaded_example_videos[word][i] = []
+            cap = cv2.VideoCapture(str(video_path))
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if ret:
+                    frame = cv2.resize(frame, (480, 270))
+                    frame = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format.Format_BGR888)
+                    self.loaded_example_videos[word][i].append(frame)
+                else:
+                    cap.release()
+
     def save_video(self):
         self._is_recording = False
         video_path, exists = get_path(self.config['data_path'], self.control_panel.select_words_cbox.currentText(),
@@ -102,17 +151,12 @@ class UiMainWindow(QtWidgets.QMainWindow):
             writer.write(frame)
         writer.release()
         self.current_video += 1
-        if self.current_video <= self.config.get('videos_per_word') - 1:
+        if self.current_video <= int(self.control_panel.num_vid_cbox.currentText()) - 1:
             self.is_in_session = True
         else:
             self.is_in_session = False
             self.current_video = 0
         self._frames_buffer = []
-
-
-
-
-
 
     @property
     def is_in_session(self):
@@ -139,7 +183,7 @@ class UiMainWindow(QtWidgets.QMainWindow):
         image = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format.Format_BGR888)
         if self._is_recording:
             self._frames_buffer.append(frame)
-            if len(self._frames_buffer) >= self.config['frames_per_vid']:
+            if len(self._frames_buffer) >= int(self.control_panel.num_frames_cbox.currentText()):
                 self.save_video()
         self.frame.setPixmap(QtGui.QPixmap(image))
 
@@ -156,7 +200,6 @@ class CaptureThread(QtCore.QThread):
         super().__init__(**kwargs)
         self.running = True
         self.video_capture = cv2.VideoCapture(camera_id)
-        print(self.video_capture.get(cv2.CAP_PROP_FPS))
         self.video_capture.set(cv2.CAP_PROP_FPS, fps)
         self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, video_size[0])
         self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, video_size[1])
@@ -170,6 +213,7 @@ class CaptureThread(QtCore.QThread):
                 print("Video capture failed")
                 break
             self.send_frame.emit(frame)
+            # self.msleep(20)
 
 
     def start_capture(self):
